541a52f9050962bd4957e6e1024d347cf0db5c94
tk <t.k.cloud9112@gmail.com>
2021-06-12 08:15:25 +0000

    first commit

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,18 @@
+def fizzbuzz(num)
+
+    if num%15 == 0
+      puts 'FizzBuzz'
+    elsif num%3 ==0
+          puts 'Fizz'
+    elsif num%5 == 0
+          puts 'Buzz'
+    else
+          puts num.to_s
+    end
+
+end
+
+num_max = 100
+(1..num_max).each do |number|
+  fizzbuzz(number)
+end
